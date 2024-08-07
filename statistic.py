b089d320c90498a11c2846cb1739e6216c72c8d6
edrikfsteiner <edrikfsteiner@gmail.com>
2024-08-07 16:25:08 -0300

    Update statistic.py

--- statistic.py
+++ statistic.py
@@ -3,7 +3,11 @@ import math
 
 class vector_storage:
     def __init__(self):
-        self.vectors = []
+        self.__vectors = []
+
+    @property
+    def vectors(self):
+        return self.__vectors
     
     def create_vec(self):
         print()
@@ -16,33 +20,33 @@ class vector_storage:
         cap = int(input("How many numbers your vector will have? "))
 
         if cap > 1:
-            vec = unord_vector(name, cap)
+            vec = vector(name, cap)
 
             while vec.last_pos != vec.capacity - 1:
                 num = float(input("Insert number: "))
                 vec.insert(num)
             
-            self.vectors.append({"Name": name, "Vector": vec.values})
+            self.vectors.append({"Name": name, "Vector": vec})
             
             return vec
         else:
             print("Your vector must have at least 2 values.")
     
-    def show_vectors(self):
+    def display_vectors(self):
         return self.vectors
 
     def get_vec(self, name):
         for i in self.vectors:
             if i["Name"] == name:
-                return i 
+                return i
                  
         return None
 
-    def update_vec(self, name, new_vec):
+    def rename_vec(self, name, new_name):
         vec_entry = self.get_vec(name)
 
         if vec_entry:
-            vec_entry["Vector"] = new_vec
+            vec_entry["Name"] = new_name
             return True
         
         return False
@@ -58,31 +62,63 @@ class vector_storage:
 
 #########################################################################################################
 
-class unord_vector:
+class vector:
     def __init__(self, name, capacity):
-        self.name = name
-        self.capacity = capacity
-        self.count = 0
-        self.last_pos = -1
-        self.values = [None] * capacity
+        self.__name = name
+        self.__capacity = capacity
+        self.__count = 0
+        self.__last_pos = -1
+        self.__values = [None] * capacity
     
-    def insert(self, value):
-        if self.last_pos == self.capacity - 1:
-            print("Full vector")
-        else:
-            self.count += 1
-            self.last_pos += 1
-            self.values[self.last_pos] = value
+    @property
+    def name(self):
+        return self.__name
     
-    def show_vector(self):
+    @name.setter
+    def name(self, name):
+        self.__name = name
+    
+    @property
+    def capacity(self):
+        return self.__capacity
+    
+    @capacity.setter
+    def capacity(self, capacity):
+        self.__capacity = capacity
+    
+    @property
+    def count(self):
+        return self.__count
+    
+    @count.setter
+    def count(self, count):
+        self.__count = count
+    
+    @property
+    def last_pos(self):
+        return self.__last_pos
+    
+    @last_pos.setter
+    def last_pos(self, last_pos):
+        self.__last_pos = last_pos
+    
+    @property
+    def values(self):
+        return self.__values
+    
+    @values.setter
+    def values(self, values):
+        self.__values = values
+
+    def display_vector(self):
         if self.last_pos == -1:
-            print("Empty vector")
+            return -1
         else:
             for i in range(self.last_pos + 1):
                 print(i, '-', self.values[i])
     
     def binary_search(self, value):
-        arr = self.merge_sort(vector.values)
+        arr = self.merge_sort(self.values)
         inf_lim = 0
         sup_lim = self.last_pos
 
@@ -98,15 +134,24 @@ class unord_vector:
                     inf_lim = current_pos + 1
                 else:
                     sup_lim = current_pos - 1
+    
+    def insert(self, value):
+        if self.last_pos == self.capacity - 1:
+            return False
+        else:
+            self.count += 1
+            self.last_pos += 1
+            self.values[self.last_pos] = value
 
     def delete(self, value):
         position = self.binary_search(value)
 
         if position == -1:
-            return -1
+            return False
         else:
             for i in range(position, self.last_pos):
                 self.values[i] = self.values[i + 1]
+
             self.count -= 1
             self.last_pos -= 1
 
@@ -157,11 +202,9 @@ class unord_vector:
         mean = self.arit_mean()
 
         for i in range(self.last_pos + 1):
-            ele = (self.values[i] - mean) ** 2
-            arr.append(ele)
+            arr.append((self.values[i] - mean) ** 2)
 
-        sum_ele = sum(arr)
-        variance = sum_ele / self.count
+        variance = sum(arr) / self.count
         return variance
 
     def use_vec(self):
@@ -169,37 +212,47 @@ class unord_vector:
             print()
             print("What do you want to know about this vector? Choose the numbers below:")
             print("+-----------------------------------------+")
-            print("1- Show vector")
-            print("2- Search element")
-            print("3- Insert element")
-            print("4- Delete element")
-            print("5- Sum of the elements")
-            print("6- Quantity of elements")
-            print("7- Sort vector")
-            print("8- Arithmetic Mean")
-            print("9- Median")
-            print("10- Mode")
-            print("11- Amplitude")
-            print("12- Variance")
-            print("13- Mean deviation")
-            print("14- Standard deviation")
-            print("15- Delete vector")
-            print("16- Add another vector")
+            print("1- Display vector")
+            print("2- Rename vector")
+            print("3- Search element")
+            print("4- Insert element")
+            print("5- Delete element")
+            print("6- Sum of the elements")
+            print("7- Quantity of elements")
+            print("8- Sort vector")
+            print("9- Arithmetic mean")
+            print("10- Median")
+            print("11- Mode")
+            print("12- Amplitude")
+            print("13- Variance")
+            print("14- Mean deviation")
+            print("15- Standard deviation")
+            print("16- Delete vector")
+            print("17- Add another vector")
             print("+-----------------------------------------+")
             print()
 
             choice = int(input("Which do you choose? "))
 
-            if choice == 1:
-                self.show_vector()
+            if choice == 1: # Display vector
+                display_vector = self.display_vector()
+
+                if display_vector == -1:
+                    print("Empty vector.")
+                else:
+                    print(display_vector)
+
+            elif choice == 2: # Rename vector
+                new_name = str(input("New name for the vector: "))
+                storage.rename_vec(self.name, new_name)
             
-            elif choice == 2:
+            elif choice == 3: # Search element
                 while True:
                     num = float(input("Which number to search? "))
                     position = self.binary_search(num)
 
                     if position == -1:
-                        print("Number was not found")
+                        print("Number was not found.")
                     else:
                         print(f"The number {num} was found in position {position}.")
 
@@ -208,74 +261,79 @@ class unord_vector:
                     if answer == 'n':
                         break
             
-            elif choice == 3:
+            elif choice == 4: # Insert element
                 while True:
                     num = float(input("Which number to insert? "))
-                    self.insert(num)
+                    insert = self.insert(num)
 
+                    if insert is False:
+                        print("Full vector, can't insert number.")
+                        break
+                    else:
+                        print(f"Number {num} was inserted.")
+                        
                     answer = str(input("Insert another one? [y/n] "))
-
+                        
                     if answer == 'n':
                         break
             
-            elif choice == 4:
+            elif choice == 5: # Delete element
                 while True:
                     num = float(input("Which number to delete? "))
-                    position = self.delete(num)
+                    delete = self.delete(num)
 
-                    if position == -1:
-                        print("Number was not found")
+                    if delete == False:
+                        print("Number was not found.")
                     else:
-                        print(f"The number {num} was deleted.")
+                        print(f"Number {num} was deleted.")
 
                     answer = str(input("Delete another one? [y/n] "))
 
                     if answer == 'n':
                         break
             
-            elif choice == 5:
+            elif choice == 6: # Sum of the elements
                 print(sum(self.values))
             
-            elif choice == 6:
+            elif choice == 7: # Quantity of elements
                 print(self.count)
             
-            elif choice == 7:
+            elif choice == 8: # Sort vector
                 print(self.merge_sort(self.values))
             
-            elif choice == 8:
+            elif choice == 9: # Arithmetic mean
                 print(self.arit_mean())
             
-            elif choice == 9:
+            elif choice == 10: # Median
                 self.merge_sort(self.values)
                 median = len(self.values) // 2
                 print(self.values[median])
 
-            elif choice == 10:
+            elif choice == 11: # Mode
                 print(self.find_mode(self.values))
 
-            elif choice == 11:
+            elif choice == 12: # Amplitude
                 print(max(self.values) - min(self.values))
 
-            elif choice == 12:
+            elif choice == 13: # Variance
                 print(self.variance())
 
-            elif choice == 13:
+            elif choice == 14: # Mean deviation
                 mean_deviation = self.variance()
                 
                 if mean_deviation < 0:
-                    mean_deviation * -1
-                    print(mean_deviation)
+                    print(mean_deviation * -1)
                 else:
                     print(mean_deviation)
 
-            elif choice == 14:
+            elif choice == 15: # Standard deviation
                 print(math.sqrt(self.variance()))
 
-            elif choice == 15:
+            elif choice == 16: # Delete vector
                 storage.delete_vec(self.name)
                 break
 
-            elif choice == 16:
+            elif choice == 17: # Add another vector
                 break
 
             else:
@@ -287,26 +345,26 @@ class unord_vector:
 storage = vector_storage()
 
 while True:
-    vector = storage.create_vec()
+    vec = storage.create_vec()
+    vector_length = len(storage.vectors)
     
-    if len(storage.vectors) == 1:
-        vector.use_vec()
+    if vector_length == 1:
+        vec.use_vec()
 
-    elif len(storage.vectors) > 1:
-        print(f"At the moment, you have {len(storage.vectors)} vectors stored.")
+    elif vector_length > 1:
+        print(f"At the moment, you have {vector_length} vectors stored.")
         print("Your vectors: ", storage.vectors)
-        choice = str(input("Do you want to compare them or see one of them? [c/s] "))
+        choice = str(input("Do you want to add one more or see one of them? [a/s] "))
 
-        if choice == 'c':
-            pass
+        if choice == 'a':
+            continue
         elif choice == 's':
             while True:
                 vec_choice = str(input("Which one? "))
                 check = storage.get_vec(vec_choice)
 
                 if check:
-                    vector.use_vec()
-                    break
+                    check["Vector"].use_vec()
                 else:
                     print("Vector was not found.")
                     continue
